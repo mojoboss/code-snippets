9c553d8231e98f8a779a1c3f364ef69e697f2d1c
Mohit Joshi <animusmojo@gmail.com>
2015-12-29 17:08:51 +0530

    Working navigation

--- My FCC projects/TwitchStatus/main.js
+++ My FCC projects/TwitchStatus/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){	
 	var users = ["freecodecamp", "medrybw", "freecodecamp", "freecodecamp"];
 
-	var offlinediv = '<div class="row"> \
+	var offlinediv = '<div class="row userdiv"> \
 			<div class="col-md-12 col-xs-12 twitch"> \
 				<div class="row"> \
 					<div class="col-md-4 col-xs-4"><img src="IMAGE" class="img-responsive image"></div> \
@@ -9,12 +9,12 @@ $(document).ready(function(){
 						<div class="row  username">USERNAME</div> \
 						<div class="row  description">DESCRIPTION</div> \
 					</div> \
-					<div class="col-md-3 col-xs-3"><i class="fa fa-exclamation-circle fa-2x redStatus"></i> </div> \
+					<div class="col-md-3 col-xs-3"><i class="fa fa-exclamation-circle fa-2x redStatus" style="color:red"></i> </div> \
 				</div> \
 			</div> \
 		</div>' ;
 
-	var onlinediv = '<div class="row"> \
+	var onlinediv = '<div class="row userdiv"> \
 			<div class="col-md-12 col-xs-12 twitch"> \
 				<div class="row"> \
 					<div class="col-md-4 col-xs-4"><img src="IMAGE" class="img-responsive image"></div> \
@@ -22,7 +22,7 @@ $(document).ready(function(){
 						<div class="row  username">USERNAME</div> \
 						<div class="row  description">DESCRIPTION</div> \
 					</div> \
-					<div class="col-md-3 col-xs-3"><i class="fa fa-check-square fa-2x greenStatus"></i> </div> \
+					<div class="col-md-3 col-xs-3"><i class="fa fa-check-square fa-2x greenStatus" style="color:green"></i> </div> \
 				</div> \
 			</div> \
 		</div>' ;
@@ -55,29 +55,49 @@ $(document).ready(function(){
 	    	}
     	});
     	allUsers.push(tempData);
+    	if(tempData.stream === null)
+    		offlineUsers.push(tempData);
+    	else
+    		onlineUsers.push(tempData);
 	}// end of for loop
-	//console.log(allUsers);
-	for(var i=0; i<allUsers.length; i++){
-		var obj = allUsers[i];
-		if(obj.stream == null){
-			offlineUsers.push(obj);
-			var temp = offlinediv;
-			temp = temp.replace("IMAGE", obj.logo);
-			temp = temp.replace("USERNAME", obj.username);
-			temp = temp.replace("DESCRIPTION", "");
-			$(".tab").append(temp);
-		}
-		else{
-			onlineUsers.push(obj);
-			var temp = onlinediv;
-			temp = temp.replace("IMAGE", obj.logo);
-			temp = temp.replace("USERNAME", obj.username);
-			temp = temp.replace("DESCRIPTION", obj.desc);
-			$(".tab").append(temp);
+	console.log(allUsers);
+
+	filldivs(allUsers);
+	$("#allbutton").click(function(){
+		$(".userdiv").remove();
+		filldivs(allUsers);
+	});
+	$("#onlinebutton").click(function(){
+		$(".userdiv").remove();
+		filldivs(onlineUsers);
+	});
+	$("#offlinebutton").click(function(){
+		$(".userdiv").remove();
+		filldivs(offlineUsers);
+	});
+	
+	//function to fill the divs with user information
+	function filldivs(users){
+		for(var i=0; i < users.length; i++){
+			var obj = users[i];
+			if(obj.stream == null){
+				var temp = offlinediv;
+				temp = temp.replace("IMAGE", obj.logo);
+				temp = temp.replace("USERNAME", obj.username);
+				temp = temp.replace("DESCRIPTION", "");
+				$(".tab").append(temp);
+			}
+			else{
+				var temp = onlinediv;
+				temp = temp.replace("IMAGE", obj.logo);
+				temp = temp.replace("USERNAME", obj.username);
+				temp = temp.replace("DESCRIPTION", obj.desc);
+				$(".tab").append(temp);
+			}
 		}
 	}
 
-	
+
 });
 
 
