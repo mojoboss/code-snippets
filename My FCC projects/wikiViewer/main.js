de079de36392477b651cc84c20d9703af73cdd79
Mohit Joshi <animusmojo@gmail.com>
2016-01-01 23:06:58 +0530

    Layout for search divs ready

--- My FCC projects/wikiViewer/main.js
+++ My FCC projects/wikiViewer/main.js
@@ -3,12 +3,14 @@ $(document).ready(function(){
 	$(".btn").click(function(){
 	  if(TriggerClick==0){
 	       TriggerClick=1;
-	       $("#div2").animate({width:'60%'}, 500);
+	       $("#div2").animate({width:'60%'}, 400);
 	  }else{
 	       TriggerClick=0;
-	       $("#div2").animate({width:'0%'}, 500);
+	       $("#div2").animate({width:'0%'}, 400);
 	  };
 	});
+
+	
 
 });
 
