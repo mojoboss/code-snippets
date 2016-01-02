f6ba586e311cfd5281e4fcf75d41b4f5d01bdacb
Mohit Joshi <animusmojo@gmail.com>
2016-01-02 16:59:43 +0530

    X or O modal created for tictactoe

---
+++ My FCC projects/ticTacToeBot/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){	
+	var playerSymbol = null;
+	var botSymbol = null;
+	$("#myModal").modal('show'); //displays the modal after the page load
+	$('#modalX').click(function(){  //click handlers for modal buttons
+		playerSymbol = 'X';
+		botSymbol = 'O';
+		console.log(playerSymbol, botSymbol, 'Xclicked');
+	});
+	$('#modalO').click(function(){
+		playerSymbol = 'O';
+		botSymbol = 'X';
+		console.log(playerSymbol, botSymbol, 'Oclicked');
+	});
+
+
+	
+});
+
+
+
