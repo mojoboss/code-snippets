6d3589821ac0b60c03671652615fa48109699a69
Mohit Joshi <animusmojo@gmail.com>
2016-01-07 14:03:50 +0530

    event handlers for buttons added

--- My FCC projects/Simon/main.js
+++ My FCC projects/Simon/main.js
@@ -2,6 +2,32 @@ $(document).ready(function(){
 	$('.mode').click(function(){
         $(this).toggleClass('modeon');
     });
+
+    //links to sound files
+    var sounds = 
+['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3',
+'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'];
+	
+	//function to play a sound
+	function play(sound){
+		var audio = new Audio(sound);
+		audio.play();
+	}
+
+	//function to respond to user click events
+	$('#green').mouseup(function(){
+		play(sounds[0]);
+	});
+	$('#red').mouseup(function(){
+		play(sounds[1]);
+	});
+	$('#yellow').mouseup(function(){
+		play(sounds[2]);
+	});
+	$('#blue').mouseup(function(){
+		play(sounds[3]);
+	});
+
 });
 
 
