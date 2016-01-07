d5963dbfa19b7e079edf0311609bed7fbd798284
Mohit Joshi <animusmojo@gmail.com>
2016-01-07 14:34:16 +0530

    Lights added in simon

--- My FCC projects/Simon/main.js
+++ My FCC projects/Simon/main.js
@@ -14,19 +14,43 @@ $(document).ready(function(){
 		audio.play();
 	}
 
+	//functions to make button produce sound and glow
+	function greenSound(){
+		$('#green').css('background-color', '#48f971');
+		play(sounds[0]);
+		window.setTimeout(function(){$('#green').css('background-color', '#00A74A');}, 500);
+	}
+	function redSound(){
+		$('#red').css('background-color', 'red');
+		play(sounds[1]);
+		window.setTimeout(function(){$('#red').css('background-color', '#9F0F17');}, 500);
+	}
+	function yellowSound(){
+		$('#yellow').css('background-color', 'yellow');
+		play(sounds[2]);
+		window.setTimeout(function(){$('#yellow').css('background-color', '#CCA707');}, 500);
+	}
+	function blueSound(){
+		$('#blue').css('background-color', '#7c83f9');
+		play(sounds[3]);
+		window.setTimeout(function(){$('#blue').css('background-color', '#094A8F');}, 500);
+	}
+
 	//function to respond to user click events
 	$('#green').mouseup(function(){
-		play(sounds[0]);
+		greenSound();
 	});
 	$('#red').mouseup(function(){
-		play(sounds[1]);
+		redSound();
 	});
 	$('#yellow').mouseup(function(){
-		play(sounds[2]);
+		yellowSound();
 	});
 	$('#blue').mouseup(function(){
-		play(sounds[3]);
+		blueSound();
 	});
+
+	
 
 });
 
